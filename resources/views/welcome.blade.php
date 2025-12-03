208669e090d312ff0af87693da6d4a2cbc98c582
rtsmn <jhonrichmonsolon29@gmail.com>
2025-12-03 18:10:26 +0800

    added favicon to the pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/svg+xml" href="{{ asset('images/communitaplogo1.svg') }}">
   <title>CommuniTap – Neighborhood Tasks, Fun Rewards</title>
 
   <!-- Google Font -->
